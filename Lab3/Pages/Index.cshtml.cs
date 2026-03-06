698479dd12ede0f746304b925ade99f81d372171
Eren Aksoy <erenaksoy6@gmail.com>
2026-03-06 15:46:13 +0300

    new project added

---
+++ Lab3/Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Lab3.Pages;
+
+public class IndexModel : PageModel
+{
+    public void OnGet()
+    {
+
+    }
+}
